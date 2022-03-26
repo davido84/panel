44b44a0afd4c8b6c20e7d5fbebab38548e39d81c
dave <davidossorio@gmail.com>
2022-03-26 12:24:58 -0700

    Deployment Settings

---
+++ panel.js
@@ -0,0 +1,21 @@
+function initPage() {
+    //document.getElementById('infoText').innerHTML = '<h3>This is the info text</h3>'
+}
+function onClickedHome() {
+    // document.getElementById('infoText').innerHTML = '<h5>You are home!</h5>'
+}
+function onClickedSchedule() {
+    document.getElementById('infoText').innerHTML = '<h5>Here is the schedule</h5>'
+}
+function onClickedTreatment() {
+    //document.getElementById('insertHere').innerHTML = '<h1>Schedule!</h1>'
+    //document.execCommand('bold', null, false);
+}
+function onClickedFAQ() {
+    //document.getElementById('insertHere').innerHTML = '<h1>Schedule!</h1>'
+    //document.execCommand('bold', null, false);
+}
+function onClickedContactUs() {
+    //document.getElementById('insertHere').innerHTML = '<h1>Schedule!</h1>'
+    //document.execCommand('bold', null, false);
+}
